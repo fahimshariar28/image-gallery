434f88183055ba758c2a0f664819d7ba972e2a57
Fahim Shariar <fahimshariar28@gmail.com>
2023-11-03 01:13:42 +0600

    image style changed

--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,4 +1,5 @@
-import { FaCheckSquare, FaRegSquare } from "react-icons/fa";
+import { FaCheckSquare } from "react-icons/fa";
+import { FaRegSquareFull } from "react-icons/fa6";
 
 const Image = ({ id, image, selectedImg, setSelectedImg, className }) => {
   const isSelected = selectedImg.includes(id);
@@ -32,8 +33,8 @@ const Image = ({ id, image, selectedImg, setSelectedImg, className }) => {
           <div className="relative">
             <img src={image} alt="" className="w-full h-full object-cover" />
             <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity">
-              <FaRegSquare
-                className="absolute top-2 left-2 text-xl text-blue-600 cursor-pointer z-10 bg-white"
+              <FaRegSquareFull
+                className="absolute top-2 left-2 text-xl cursor-pointer z-10 bg-white"
                 onClick={handleSelect}
               />
             </div>
