02023a8864bc9c58264a5611de5660705cb8cc57
Fahim Shariar <fahimshariar28@gmail.com>
2023-11-03 16:29:55 +0600

    Drag and drop sorting with Dnd kit

--- src/App.jsx
+++ src/App.jsx
@@ -2,6 +2,8 @@ import "./App.css";
 import { useState } from "react";
 import Header from "./components/Header";
 import Image from "./components/Image";
+import { DndContext } from "@dnd-kit/core";
+import { arrayMove, SortableContext } from "@dnd-kit/sortable";
 import { FaImage } from "react-icons/fa";
 
 function App() {
@@ -29,33 +31,46 @@ function App() {
     setImages(updatedImages);
   };
 
-  return (
-    <div className="bg-gray-400 h-full m-0 p-10">
-      <div className="bg-white rounded-lg">
-        {/* Header Section */}
-        <Header onDelete={handleDelete} selectedImg={selectedImg}></Header>
+  const handleDragEnd = ({ active, over }) => {
+    if (active.id !== over.id) {
+      setImages((prevImages) => {
+        const oldIndex = prevImages.findIndex((img) => img.id === active.id);
+        const newIndex = prevImages.findIndex((img) => img.id === over.id);
 
-        {/* Image Section */}
-        <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5  gap-4 p-4">
-          {images.map((image, index) => (
-            <Image
-              key={image.id}
-              image={image.image}
-              id={image.id}
-              selectedImg={selectedImg}
-              setSelectedImg={setSelectedImg}
-              className={index === 0 ? "col-span-2 row-span-2" : ""}
-            ></Image>
-          ))}
-          <div className="bg-gray-50 border-dashed border-gray-200 border-2 flex flex-col gap-4 items-center justify-center rounded-lg p-10 cursor-pointer">
-            <FaImage className="h-7 w-7 text-gray-800" />
-            <p className="text-black text-center text-lg font-medium">
-              Add Image
-            </p>
-          </div>
+        return arrayMove(prevImages, oldIndex, newIndex);
+      });
+    }
+  };
+
+  return (
+    <DndContext onDragEnd={handleDragEnd}>
+      <div className="bg-gray-400 h-full m-0 p-10">
+        <div className="bg-white rounded-lg">
+          <Header onDelete={handleDelete} selectedImg={selectedImg}></Header>
+
+          <SortableContext items={images.map((image) => image.id)}>
+            <div className="grid grid-cols-2 md:grid-cols-3 lg:grid-cols-5 gap-4 p-4">
+              {images.map((image, index) => (
+                <Image
+                  key={image.id}
+                  image={image.image}
+                  id={image.id}
+                  selectedImg={selectedImg}
+                  setSelectedImg={setSelectedImg}
+                  className={index === 0 ? "col-span-2 row-span-2" : ""}
+                ></Image>
+              ))}
+              <div className="bg-gray-50 border-dashed border-gray-200 border-2 flex flex-col gap-4 items-center justify-center rounded-lg p-10 cursor-pointer">
+                <FaImage className="h-7 w-7 text-gray-800" />
+                <p className="text-black text-center text-lg font-medium">
+                  Add Image
+                </p>
+              </div>
+            </div>
+          </SortableContext>
         </div>
       </div>
-    </div>
+    </DndContext>
   );
 }
 
--- src/components/Image.jsx
+++ src/components/Image.jsx
@@ -1,10 +1,23 @@
 import { FaCheckSquare } from "react-icons/fa";
 import { FaRegSquareFull } from "react-icons/fa6";
+import { useSortable } from "@dnd-kit/sortable";
+import { CSS } from "@dnd-kit/utilities";
 
 const Image = ({ id, image, selectedImg, setSelectedImg, className }) => {
   const isSelected = selectedImg.includes(id);
 
-  const handleSelect = () => {
+  const {
+    attributes,
+    listeners,
+    setNodeRef,
+    transform,
+    transition,
+    isDragging,
+  } = useSortable({ id });
+
+  const handleClick = () => {
+    if (isDragging) return;
+
     if (isSelected) {
       setSelectedImg(selectedImg.filter((imgId) => imgId !== id));
     } else {
@@ -14,13 +27,22 @@ const Image = ({ id, image, selectedImg, setSelectedImg, className }) => {
 
   return (
     <div
+      ref={setNodeRef}
+      style={{
+        transform: CSS.Transform.toString(transform),
+        transition,
+        zIndex: isSelected || isDragging ? 2 : 1,
+      }}
+      {...attributes}
+      {...listeners}
+      onClick={handleClick}
       className={`relative rounded-lg overflow-hidden border border-gray-500 ${className}`}
     >
       {isSelected ? (
         <>
           <FaCheckSquare
             className="absolute top-2 left-2 text-xl rounded-lg text-blue-600 cursor-pointer z-40"
-            onClick={handleSelect}
+            onClick={handleClick}
           />
           <img
             src={image}
@@ -35,7 +57,7 @@ const Image = ({ id, image, selectedImg, setSelectedImg, className }) => {
             <div className="absolute inset-0 bg-black opacity-0 hover:opacity-50 transition-opacity">
               <FaRegSquareFull
                 className="absolute top-2 left-2 text-xl cursor-pointer z-10 bg-white"
-                onClick={handleSelect}
+                onClick={handleClick}
               />
             </div>
           </div>
